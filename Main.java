91c6ebd73207a5530bc45436e3dd3f2c330d729a
lunerwings <cillian2006@gmail.com>
2026-04-08 12:24:40 -0400

    added the ability to change input into tokens

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-public class Main{
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) {
-        Tokenizer tokens = new Tokenizer();
+        String[] tokens;
+        String[][] nums;
+        Tokenizer fromString = new Tokenizer();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the expression : ");
+        String starterTokens = scan.nextLine();
+        tokens = fromString.intoArray(starterTokens);
+        System.out.println(Arrays.toString(tokens));
     }
 }
+
--- Tokenizer.java
+++ Tokenizer.java
@@ -1,5 +1,47 @@
 public class Tokenizer {
-    public String[] toTokens(String a){
-        
+    private String[] finalTokens;
+    private String[] finalTokensClean;
+    private int counter = 0;
+    public Tokenizer(){}
+    public String[] intoArray(String a){
+        a = a.replace(" ","");
+        finalTokens = new String[a.length()];
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)=='{'||a.charAt(i)=='['||a.charAt(i)=='(') {
+                finalTokens[i]=a.charAt(i)+"";
+            }else if (a.charAt(i)=='}'||a.charAt(i)==']'||a.charAt(i)==')') {
+                finalTokens[i]=a.charAt(i)+"";
+            }else if (a.charAt(i)=='+'||a.charAt(i)=='-'||a.charAt(i)=='*'||a.charAt(i)=='/') {
+                finalTokens[i] = a.charAt(i)+"";
+            }else if(a.charAt(i)!=' '){
+                if (a.charAt(i) == '1' || a.charAt(i) == '2' || a.charAt(i) == '3' || a.charAt(i) == '4' || a.charAt(i) == '5' || a.charAt(i) == '6' || a.charAt(i) == '7' || a.charAt(i) == '8' || a.charAt(i) == '9') {
+                    finalTokens[i] = a.charAt(i) + "";
+                    counter = i;
+                    for(int j = counter+1; j < a.length(); j++) {
+                        if (a.charAt(j) == '1' || a.charAt(j) == '2' || a.charAt(j) == '3' || a.charAt(j) == '4' || a.charAt(j) == '5' || a.charAt(j) == '6' || a.charAt(j) == '7' || a.charAt(j) == '8' || a.charAt(j) == '9') {
+                            finalTokens[counter] = finalTokens[counter]+ a.charAt(j);
+                            i++;
+                        }else break;
+                    }
+                } else{
+                    finalTokens[i] = a.charAt(i) + "";
+                }
+            }
+        }
+        counter = 0;
+        for(int i = 0; i < finalTokens.length; i++){
+            if(finalTokens[i] != null){
+                counter++;
+            }
+        }
+        finalTokensClean = new String[counter];
+        counter = 0;
+        for(int i = 0; i < finalTokens.length; i++){
+            if(finalTokens[i] != null){
+                finalTokensClean[counter] = finalTokens[i];
+                counter++;
+            }
+        }
+        return finalTokensClean;
     }
 }

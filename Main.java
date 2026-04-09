825143ea54cfcea18218ec0fe7b0dce48866a565
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2026-04-09 16:58:24 -0400

    refactor(main): ternary operator, more todo items
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- Main.java
+++ Main.java
@@ -19,14 +19,11 @@ public class Main {
         var parser = new Parser();
         try {
             var expr = parser.parseExpr(tokens);
-            System.out.println("Placeholder printed value:");
-            if(expr == null) {
-                System.out.println("");
-            } else {
-                System.out.println(expr);
-            }
+            System.out.println("Placeholder printed value: " + ((expr == null)
+                ? "empty"
+                : expr.toString()));
 
-            System.out.println("Eval: " + expr.eval());
+            System.out.println("Eval: " + ((expr == null) ? "empty" : expr.eval()));
         } catch(Exception e) {
             System.out.println("Exception!!!: " + e);
             e.printStackTrace();
@@ -34,7 +31,7 @@ public class Main {
     }
 
     private static void printTokens(List<Token> tokens) {
-        System.out.print("[");
+        System.out.print("Tokens: [");
         if(tokens.size() >= 2) {
             System.out.print(tokens.get(0));
         }
--- TODO.md
+++ TODO.md
@@ -3,6 +3,7 @@
 - [x] Working lexical analyzer.
   - Note: the only purpose is to break the input string into tokens. The
   lexical analyzer doesn't care if the input is a valid expression.
+  - [ ] We might need to account for negative numbers, say, in `-1 + 2`.
 - [x] Working parser, for the valid expressions.
 - [ ] Improve error reporting for invalid expressions.
   - [ ] For `Token`s, also save the index of the token in the input string.
@@ -12,7 +13,9 @@
     - [ ] Unexpected token.
     - [ ] Expect token, but hit `EOF`.
 - [ ] Evaluator
+  - [ ] Current version kind of works, but because of division, we really want
+  an `eval` returning `float` or `double` instead.
 - [ ] Improve the AST printer. Currently, it's printed in Lisp form. We want
 the tree form as shown in the assignment sheet(?).
 - [ ] And we're done.
-- [ ] Maybe attach an AGPL-3-or-later to annoy the professor.
+- [ ] Maybe attach an AGPL-3.0-or-later to annoy the professor.

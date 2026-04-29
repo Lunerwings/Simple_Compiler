6bf5360ec36c79dad4c19cae968b4a5ff5259fde
lunerwings <cillian2006@gmail.com>
2026-04-29 10:41:11 -0400

    Seperated into rows (still working on printing tree)

--- Main.java
+++ Main.java
@@ -19,9 +19,12 @@ public class Main {
         var parser = new Parser();
         try {
             var expr = parser.parseExpr(tokens);
+            var strExpr = expr.toString();
+            printParseTree(strExpr);
+            System.out.println("Parsed expression: " + expr);
             System.out.println("Placeholder printed value: " + ((expr == null)
-                ? "empty"
-                : expr.toString()));
+                    ? "empty"
+                    : expr.toString()));
 
             System.out.println("Eval: " + ((expr == null) ? "empty" : expr.eval()));
         } catch(Exception e) {
@@ -41,25 +44,55 @@ public class Main {
         System.out.println("]");
     }
 
-    private static void printParseTree(String[][] sortedTokens){
-        for(int i = 0; i < 2*sortedTokens[0].length-1; ++i) {
-            System.out.print(" ");
+    private static void printParseTree(String expr){
+        expr = expr.replaceAll("\\s","");
+        int counter = -1;
+        int highestCounter = -1;
+        ArrayList<String> tokens = new ArrayList<>();
+        for(int i = 0; i < expr.length(); ++i) {
+            tokens.add(expr.substring(i, i + 1));
         }
-        System.out.println(sortedTokens[0][0]);
-        for(int i = 1; i < (2*sortedTokens[0].length -1)/2; ++i) {
-            for(int j = 0; j < 2*sortedTokens[0].length-2*i; ++j) {
-                System.out.print(" ");
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        list.add(new ArrayList<String>());
+        System.out.println("this is the tokens: " + tokens);
+        for(int i = 0; i < tokens.size(); ++i) {
+
+            if (tokens.get(i).equals("(") && (i==0||!tokens.get(i - 1).equals(")"))) {
+                counter ++;
+                if (counter > highestCounter) {
+                    highestCounter = counter;
+                    list.add(new ArrayList<String>());
+                }
+            } else if (tokens.get(i).equals(")")) {
+                counter--;
+            } else if (tokens.get(i).equals("*") || tokens.get(i).equals("/")
+                    || tokens.get(i).equals("+") || tokens.get(i).equals("-")) {
+                list.get(counter).add(tokens.get(i));
+                if(!tokens.get(i+1).equals("("))
+                    counter++;
+                if(counter > highestCounter) {
+                    highestCounter = counter;
+                    list.add(new ArrayList<String>());
+                }
+            }else{
+                for(int j = 0; j < 10;j++){
+                    if(String.valueOf(j).equals(tokens.get(i))
+                            &&!String.valueOf(j+3).equals(tokens.get(i))){
+                        list.get(counter).add(tokens.get(i));
+                    };
+                }
             }
-            System.out.println("/ \\");
-            for(int j = 0; j < 2*sortedTokens[0].length-1-i*2; ++j) {
-                System.out.print(" ");
+        }
+        System.out.println(list+"\n\n\n\n\n\n\n\n");
+        for(int i = 0; i < list.size(); ++i) {
+            for(int j = 0; j < list.get(i).size(); ++j) {
+                System.out.print(list.get(i).get(j) + " ");
             }
-            System.out.print(sortedTokens[0][i]);
-            for(int j = 0; j < 3; ++j) {
-                System.out.print(" ");
-            }
-            System.out.println(sortedTokens[1][i-1]);
+            System.out.println();
         }
     }
 }
 
+//(3 + 2)- 5*6
+//(3*2)*6-4*5+6*7+(4*7)
+//(3+2)*(6+7)

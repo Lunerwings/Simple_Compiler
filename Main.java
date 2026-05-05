f34deac96558ea9eedc85596b2e51cc640325d4f
lunerwings <cillian2006@gmail.com>
2026-05-05 00:02:08 -0400

    sorted into tree levels and there corresponding location in the level.

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         var tokenizer = new Tokenizer();
         var scanner = new Scanner(System.in);
         System.out.print("Enter the expression: ");
@@ -20,22 +20,16 @@ public class Main {
         try {
             var expr = parser.parseExpr(tokens);
             var strExpr = expr.toString();
-            // printParseTree(strExpr);
-            // System.out.println("Parsed expression: " + expr);
+            printParseTree(strExpr);
+            System.out.println("Parsed expression: " + expr);
             System.out.println("Placeholder printed value: " + ((expr == null)
                     ? "empty"
                     : expr.toString()));
-            // ArrayList<StringBuilder> lines = new ArrayList<>();
-            // lines.add(new StringBuilder());
-            // printWithSpaces(expr, 0, 0, lines);
-            // for(var lnum = 0; lnum < lines.size(); ++lnum) {
-            //     System.out.println(lines.get(lnum).toString());
-            // }
 
             System.out.println("Eval: " + ((expr == null) ? "empty" : expr.eval()));
         } catch(Exception e) {
-            // throw e;
             System.out.println("Exception!!!: " + e.getMessage());
+            e.printStackTrace();
             System.exit(69);
         }
     }
@@ -43,7 +37,7 @@ public class Main {
     private static void printTokens(List<Token> tokens) {
         System.out.print("Tokens: [");
         if(tokens.size() >= 2) {
-            System.out.print(tokens.get(0));
+            System.out.print(tokens.getFirst());
         }
         for(int i = 1; i < tokens.size() - 1; ++i) {
             System.out.print(", " + tokens.get(i));
@@ -53,53 +47,125 @@ public class Main {
 
     private static void printParseTree(String expr){
         expr = expr.replaceAll("\\s","");
-        int counter = -1;
+        int counter = 0;
         int highestCounter = -1;
         ArrayList<String> tokens = new ArrayList<>();
         for(int i = 0; i < expr.length(); ++i) {
             tokens.add(expr.substring(i, i + 1));
         }
         ArrayList<ArrayList<String>> list = new ArrayList<>();
-        list.add(new ArrayList<String>());
         System.out.println("this is the tokens: " + tokens);
         for(int i = 0; i < tokens.size(); ++i) {
-
-            if (tokens.get(i).equals("(") && (i==0||!tokens.get(i - 1).equals(")"))) {
-                counter ++;
-                if (counter > highestCounter) {
-                    highestCounter = counter;
-                    list.add(new ArrayList<String>());
-                }
-            } else if (tokens.get(i).equals(")")) {
-                counter--;
-            } else if (tokens.get(i).equals("*") || tokens.get(i).equals("/")
-                    || tokens.get(i).equals("+") || tokens.get(i).equals("-")) {
-                list.get(counter).add(tokens.get(i));
-                if(!tokens.get(i+1).equals("("))
+            if(i >= 2 &&i <= tokens.size() - 3) {
+                if ((tokens.get(i-1).equals("+")||tokens.get(i-1).equals("-")
+                        ||tokens.get(i-1).equals("*")||tokens.get(i-1).equals("/"))
+                        &&!(tokens.get(i).equals("*")||tokens.get(i).equals("/")||tokens.get(i).equals("+")||tokens.get(i).equals("-")||
+                        tokens.get(i).equals("(")||tokens.get(i).equals(")"))) {
                     counter++;
-                if(counter > highestCounter) {
+
+                    if (counter > highestCounter) {
+
+                        highestCounter = counter;
+                        list.add(new ArrayList<>());
+                        for(int j = 0; j < Math.pow(2,highestCounter-1); ++j) {
+                            list.get(highestCounter-1).add(null);
+                        }
+
+                    }
+                }
+            }
+            if(i>1 && (tokens.get(i-2).equals("*")||tokens.get(i-2).equals("/")||tokens.get(i-2).equals("+") ||tokens.get(i-2).equals("-"))&&
+                    (tokens.get(i).equals("*")||tokens.get(i).equals("/")||tokens.get(i).equals("+") ||tokens.get(i).equals("-"))
+                    &&tokens.get(i-1).equals("(")) {
+                counter++;
+
+                if (counter > highestCounter) {
+
                     highestCounter = counter;
-                    list.add(new ArrayList<String>());
+                    list.add(new ArrayList<>());
+                    for(int j = 0; j < Math.pow(2,highestCounter-1); ++j) {
+                        list.get(highestCounter-1).add(null);
+                    }
+
                 }
-            }else{
-                for(int j = 0; j < 10;j++){
-                    if(String.valueOf(j).equals(tokens.get(i))
-                            &&!String.valueOf(j+3).equals(tokens.get(i))){
-                        list.get(counter).add(tokens.get(i));
-                    };
+            }
+            if (tokens.get(i).equals(")")) {
+
+                counter--;
+
+            }
+            if ((tokens.get(i).equals("*") || tokens.get(i).equals("/")
+                    || tokens.get(i).equals("+") || tokens.get(i).equals("-"))) {
+
+
+                counter++;
+
+
+
+
+
+                if(counter > highestCounter) {
+
+                    highestCounter = counter;
+                    list.add(new ArrayList<>());
+                    for(int j = 0; j < Math.pow(2,highestCounter-1); ++j) {
+                        list.get(highestCounter-1).add(null);
+                    }
+                }
+
+            }
+            if(i>1&&((tokens.get(i).equals("*")||tokens.get(i).equals("/")||
+                    tokens.get(i).equals("+")||tokens.get(i).equals("-"))
+                    &&tokens.get(i-1).equals("("))) {
+                counter--;
+            }
+
+
+            if(!(tokens.get(i).equals(")")||tokens.get(i).equals("("))) {
+
+                int space = 0;
+
+
+                if (counter == 1) {
+
+
+                    list.get(0).set(0, tokens.get(i));
+
+
+                }else{
+
+                    for(int j = 0; j < list.get(counter-2).size();j++){
+
+                        if(list.get(counter-2).get(j)!=null) {
+                            space++;
+                        }
+                    }
+
+
+                    if(list.get(counter-1).get(2*(space-1)) == null) {
+
+                        list.get(counter - 1).set(2*(space-1), tokens.get(i));
+
+
+                    }else{
+
+
+                        list.get(counter - 1).set(2*(space-1)+1, tokens.get(i));
+
+
+                    }
                 }
             }
         }
-        System.out.println(list+"\n\n\n\n\n\n\n\n");
-        for(int i = 0; i < list.size(); ++i) {
-            for(int j = 0; j < list.get(i).size(); ++j) {
-                System.out.print(list.get(i).get(j) + " ");
-            }
-            System.out.println();
-        }
+
+        System.out.println(list);
+
+
     }
 }
 
 //(3 + 2)- 5*6
+//1+(3*6)
 //(3*2)*6-4*5+6*7+(4*7)
 //(3+2)*(6+7)
+//(3*6)+1+(3*6)*7)
